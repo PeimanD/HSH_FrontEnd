30d3d65a5c7945fb679e52944cc67157336b9569
SimonWuBCIT <swu111@my.bcit.ca>
2019-05-03 00:54:50 -0700

    Added Graph component as well as styling and some container fixes

--- src/components/ContentContainer.jsx
+++ src/components/ContentContainer.jsx
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Stats from "./Stats";
 import ThermoCardContainer from "./ThermoCardContainer";
 
+import "./css/content.css";
+
 class ContentContainer extends Component {
   render() {
     return (
-      <div>
+      <div className="d-flex flex-grow-1 content-background">
         <Switch>
           <Route exact path="/thermoCards" component={ThermoCardContainer} />
           <Route exact path="/stats" component={Stats} />
