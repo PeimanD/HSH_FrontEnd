2a03d14e36bcc5df004f7ab4ed2e7ac6a5807582
Daniel Shin <danielshin234@gmail.com>
2019-05-04 06:28:17 -0700

    done thermostat page

--- src/components/user/thermostats/Thermostat.jsx
+++ src/components/user/thermostats/Thermostat.jsx
@@ -1,10 +1,64 @@
 import React, {Component} from "react";
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
 
 class Thermostat extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            id: this.props.id,
+            status: this.props.status,
+            mode: this.props.mode,
+            setTemp: this.props.setTemp,
+            currentTemp: this.props.currentTemp
+        };
     };
+
+    render() {
+        const {classes} = this.props;
+        return (
+            <Card className={classes.card}>
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                        Thermostat #{this.state.id}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Status: {this.state.status ? 'Automatic' : 'Manual'}
+                    </Typography>
+                    <Typography component="p">
+                        Set Temperature: {this.state.setTemp}
+                    </Typography>
+                    <Typography component="p">
+                        Current Temperature: {this.state.currentTemp}
+                    </Typography>
+                </CardContent>
+            </Card>
+        );
+    }
 }
 
-export default Thermostat;
+const styles = {
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+};
+
+Thermostat.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Thermostat);
--- src/components/user/thermostats/Thermostats.jsx
+++ src/components/user/thermostats/Thermostats.jsx
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid/index';
 import Thermostat from "./Thermostat.jsx";
 import SideNav from '../SideNav'
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core";
+import Paper from '@material-ui/core/Paper';
 
 class Thermostats extends Component {
     constructor(props) {
@@ -19,18 +22,73 @@ class Thermostats extends Component {
     };
 
     render() {
+        const {classes} = this.props;
+
         return (
             <div>
                 <SideNav/>
-                <Grid container>
-                    <Grid item xs={6}><Thermostat/></Grid>
-                    <Grid item xs={6}><Thermostat/></Grid>
-                    <Grid item xs={6}><Thermostat/></Grid>
-                    <Grid item xs={6}><Thermostat/></Grid>
-                </Grid>
+                <div className={classes.root}>
+                    <Paper className={classes.paper}>
+                        <Grid container spacing={8}>
+                            <Grid item xs={6}>
+                                <Thermostat
+                                    id={1}
+                                    status={true}
+                                    mode={true}
+                                    setTemp={20}
+                                    currentTemp={30}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Thermostat
+                                    id={2}
+                                    status={true}
+                                    mode={true}
+                                    setTemp={20}
+                                    currentTemp={30}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Thermostat
+                                    id={3}
+                                    status={false}
+                                    mode={true}
+                                    setTemp={20}
+                                    currentTemp={30}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Thermostat
+                                    id={4}
+                                    status={false}
+                                    mode={true}
+                                    setTemp={20}
+                                    currentTemp={30}
+                                />
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </div>
             </div>
         );
     }
 }
 
-export default Thermostats;
+const styles = {
+    root: {
+        marginLeft: '240px'
+    },
+    paper: {
+        height: '50%',
+        width: '100%',
+        marginTop: '10%',
+        borderRadius: '15px',
+        textAlign: 'center',
+    },
+};
+
+Thermostats.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Thermostats);
