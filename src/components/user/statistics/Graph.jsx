d543b982a831feb04cac1889d81d56279d7510ad
SimonWuBCIT <swu111@my.bcit.ca>
2019-05-10 13:53:31 -0700

    Implemented temp logic to push fake data to db

--- src/components/user/statistics/Graph.jsx
+++ src/components/user/statistics/Graph.jsx
@@ -2,13 +2,14 @@ import axios from "axios";
 import React, { Component } from 'react';
 
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+import Button from '@material-ui/core/Button/index';
 import { get } from "https";
 
 import './graph.css';
 
 import { Labels } from './string.js';
 
-const hours = ["00:00", "01:00", "02:00", "03:00", "04:00","05:00", "06:00","07:00", "08:00","09:00", "10:00","11:00", "12:00","13:00", "14:00","15:00", "16:00","17:00", "18:00","19:00", "20:00","21:00", "22:00", "23:00"]
+const hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
 const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
@@ -39,12 +40,42 @@ class Graph extends Component {
       this.populateData(24, hours);
    };
 
+   injectData = () => {
+      let monthDaysCount = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      let data = [];
+
+      for (let k = 0; k < 12; ++k) {
+         for (let j = 0; j < monthDaysCount[k]; ++j) {
+            let log = {
+               year: 2018,
+               month: 1,
+               day: 1,
+               thermostatId: "",
+               masterDevId: "",
+               cTemps: [],
+               oTemps: [],
+               sTemps: [],
+            }
+            log.month = k + 1;
+            log.day = j + 1;
+            for (let i = 0; i < 144; ++i) {
+               log.sTemps[i] = 20 + this.getRandomInt(2);
+               log.cTemps[i] = log.sTemps[i] - 1 - this.getRandomInt(3);
+               log.oTemps[i] = log.sTemps[i] - 12 - this.getRandomInt(2);
+            }
+            data.push(log);
+         }
+      }
+
+      console.log(data);
+   }
+
    populateData = (amount, unit) => {
       let data = [];
       // will need to replace the for loop with axios.get request to get data from mongodb
       // use this,props.currentThermostat to find out which thermostat we want
       for (let i = 0; i < amount; ++i) {
-         let tempData = {name: '', sTemp: 0, cTemp: 0, oTemp: 0};
+         let tempData = { name: '', sTemp: 0, cTemp: 0, oTemp: 0 };
          tempData.name = unit[i];
          tempData.sTemp = 20 + this.getRandomInt(2);
          tempData.cTemp = tempData.sTemp - 1 - this.getRandomInt(3);
@@ -53,7 +84,7 @@ class Graph extends Component {
       }
 
       // update state with the data, 
-      this.setState({graphData: data, currentThermostat: this.props.currentThermostat, granularity: unit});
+      this.setState({ graphData: data, currentThermostat: this.props.currentThermostat, granularity: unit });
    }
 
    getRandomInt = max => {
@@ -87,7 +118,7 @@ class Graph extends Component {
       let dateArray = [];
       let numDays = new Date(date.getFullYear(), curMonth + 1, 0).getDate();
       for (let i = 0; i < numDays; ++i) {
-         dateArray[i] = months[curMonth] + " " + (i + 1); 
+         dateArray[i] = months[curMonth] + " " + (i + 1);
       }
       return dateArray;
    }
@@ -107,8 +138,8 @@ class Graph extends Component {
             <ResponsiveContainer>
                <LineChart data={this.state.graphData}>
                   <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                  <XAxis ticks={this.state.granularity} dataKey="name" interval={this.state.graphInterval}/>
-                  <YAxis label={{ value: 'Temperature (\xB0C)', angle: -90, position: 'center', dx: -15}} />
+                  <XAxis ticks={this.state.granularity} dataKey="name" interval={this.state.graphInterval} />
+                  <YAxis label={{ value: 'Temperature (\xB0C)', angle: -90, position: 'center', dx: -15 }} />
                   <Tooltip />
                   <Legend />
 
@@ -117,6 +148,7 @@ class Graph extends Component {
                   <Line type="monotone" dataKey="oTemp" stroke="dodgerblue" />
                </LineChart>
             </ResponsiveContainer>
+            <Button onClick={this.injectData}>Upload Fake Data</Button>
          </div>
       );
    };
