25a56549b6fb2ad9158f82127dae25e61975e992
SimonWuBCIT <swu111@my.bcit.ca>
2019-05-15 15:21:04 -0700

    Modified bad data page

--- src/components/user/statistics/BadData.jsx
+++ src/components/user/statistics/BadData.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {withStyles} from '@material-ui/core/styles/index';
 import './graph.css';
 
-let BadData = props => (
-    <div className="graph-inner-container">
-        <p>Invalid Data Requested</p>
-    </div>
-);
+const style = theme => ({
+    paper: {
+        width: '70vw',
+        height: '50vh',
+        borderRadius: '15px',
+        backgroundColor: 'smokewhite',
+        textAlign: 'center',
+        margin: 'auto',
+    },
+    innerText: {
+        position: 'relative',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+    }
+});
+
+let BadData = props => {
+    const {classes} = props;
+    return (
+        <div className={classes.paper}>
+            <p className={classes.innerText}>No Data Found</p>
+        </div>
+    );
+};
 
 
-export default BadData;
+export default withStyles(style)(BadData);
